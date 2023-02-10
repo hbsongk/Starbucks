83b6fc3aa62e95cb6e0bf18b8a53676c98628a88
hbsongk <68498323+hbsongk@users.noreply.github.com>
2023-02-10 16:54:28 +0900

    add main image animation

--- js/main.js
+++ js/main.js
@@ -38,3 +38,12 @@ window.addEventListener(
   }, 300)
 );
 // _.throttle(함수, 시간)
+
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach(function (fadeEl, index) {
+  // gasp.to(요소, 지속시간, 옵션)
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  });
+});
