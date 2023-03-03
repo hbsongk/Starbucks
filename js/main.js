50c8c6d52068626aeeacdd99b6f493d12ffa5a6c
hbsongk <68498323+hbsongk@users.noreply.github.com>
2023-03-03 14:13:53 +0900

    add footer design logic

--- js/main.js
+++ js/main.js
@@ -124,3 +124,6 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, "show")
     .addTo(new ScrollMagic.Controller());
 });
+
+const thisYear = document.querySelector(".this-year");
+thisYear.textContent = new Date().getFullYear();
