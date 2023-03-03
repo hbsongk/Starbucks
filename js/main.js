153f87644423dcfee6fcc13452c4b7511d048594
hbsongk <68498323+hbsongk@users.noreply.github.com>
2023-03-03 14:42:19 +0900

    add footer to-top button logic

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ searchInputEl.addEventListener("blur", function () {
 });
 
 const badgeEl = document.querySelector("header .badges");
-
+const toTopEl = document.querySelector("#to-top");
 window.addEventListener(
   "scroll",
   _.throttle(function () {
@@ -28,17 +28,29 @@ window.addEventListener(
         opacity: 0,
         display: "none",
       });
+      // 버튼 보이기!
+      gsap.to(toTopEl, 0.2, {
+        x: 0,
+      });
     } else {
       // 배지 보이기
       gsap.to(badgeEl, 0.6, {
         opacity: 1,
         display: "block",
       });
+      // 버튼 숨기기!
+      gsap.to(toTopEl, 0.2, {
+        x: 100,
+      });
     }
   }, 300)
 );
 // _.throttle(함수, 시간)
-
+toTopEl.addEventListener("click", function () {
+  gsap.to(window, 0.7, {
+    scrollTo: 0,
+  });
+});
 const fadeEls = document.querySelectorAll(".visual .fade-in");
 fadeEls.forEach(function (fadeEl, index) {
   // gasp.to(요소, 지속시간, 옵션)
